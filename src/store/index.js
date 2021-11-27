18fef8c142f4a197283aab97627373db58694017
Derek Moore <moorder1112@gmail.com>
2021-11-27 09:39:21 -0600

    removed incorrect code in vuex stores

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import bulbs from "./modules/bulbs";
 import axios from "axios";
-import PrimeVue from "primevue/config";
-import Accordion from "primevue/accordion";
-import AccordionTab from "primevue/accordiontab";
 
 Vue.use(Vuex);
-Vue.use(PrimeVue);
-
-Vue.component("Accordion", Accordion);
-Vue.component("AccordionTab", AccordionTab);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {
-    bulbs,
-  },
+  modules: {},
 });
-//'http://192.168.1.133:5000';
-axios.defaults.baseURL = "http://localhost:5000";
+
+// axios.defaults.baseURL = "http://localhost:5000";
 axios.interceptors.response.use(
   (response) => {
     return response;
